82928d46218de4f9e136417ae9dc28224986ab5d
dlxzeus777 <silverchannelcontact@gmail.com>
2023-01-05 20:59:58 +0100

    changes

--- src/Projects.js
+++ src/Projects.js
@@ -9,6 +9,7 @@ import huddle from './images/huddle-landing.jpg'
 import dashboard from './images/time-tracker-dashboard.png'
 import stoic from './images/stoic.png'
 import ProjectCards from './ProjectCards'
+import cryptocloud from './images/cryptocloud.png'
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
@@ -25,60 +26,67 @@ const Projects = ({ theme }) => {
   const projects = [
     {
       id: 1,
+      image: cryptocloud,
+      title: 'Crypto Cloud',
+      live: 'https://dlxzeus777.github.io/Crypto-Cloud/',
+      code: 'https://github.com/dlxzeus777/Crypto-Cloud'
+    },
+    {
+      id: 2,
       image: tenzies,
       title: 'Tenzies App',
       live: 'https://dlxzeus777.github.io/tenzies-game-react/',
       code: 'https://github.com/dlxzeus777/tenzies-game-react'
     },
     {
-      id: 2,
+      id: 3,
       image: tipCalculator,
       title: 'Tip Calculator App',
       live: 'https://dlxzeus777.github.io/tip-calculator-app/',
       code: 'https://github.com/dlxzeus777/tip-calculator-app'
     },
     {
-      id: 3,
+      id: 4,
       image: meme,
       title: 'Random Meme Generator',
       live: 'https://dlxzeus777.github.io/meme-generator/',
       code: 'https://github.com/dlxzeus777/meme-generator'
     },
     {
-      id: 4,
+      id: 5,
       image: fylo,
       title: 'Fylo Landing Page',
       live: 'https://dlxzeus777.github.io/flyo-dark-theme-landing-page/',
       code: 'https://github.com/dlxzeus777/flyo-dark-theme-landing-page'
     },
     {
-      id: 5,
+      id: 6,
       image: huddle,
       title: 'Huddle Landing Page',
       live: 'https://dlxzeus777.github.io/Huddle-landing-page-with-curved-sections/',
       code: 'https://github.com/dlxzeus777/Huddle-landing-page-with-curved-sections'
     },
     {
-      id: 6,
+      id: 7,
       image: dashboard,
       title: 'Time Tracking Dashboard',
       live: 'https://dlxzeus777.github.io/Time-tracking-dashboard/',
       code: 'https://github.com/dlxzeus777/Time-tracking-dashboard'
     },
     {
-      id: 7,
+      id: 8,
       image: fylo2,
       title: 'Fylo Landing Page',
       live: 'https://dlxzeus777.github.io/Fylo-landing-page-with-two-column-layout/',
       code: 'https://github.com/dlxzeus777/Fylo-landing-page-with-two-column-layout'
     },
     {
-      id: 8,
+      id: 9,
       image: stoic,
       title: 'Stoic Quote Generator',
       live: 'https://dlxzeus777.github.io/stoic-quotes/',
       code: 'https://github.com/dlxzeus777/stoic-quotes'
-    }
+    },
   ]
 
 
